2d5d13035a7e4ccb033bd01c52975044ffbcd95d
Oleg Naumov <oleg.naumov@gmail.com>
2017-04-22 13:22:37 -0400

    missing semicolons

--- setWidth.js
+++ setWidth.js
@@ -1,13 +1,13 @@
 function setTextWidth(){
-    document.querySelector(".story-detail-content").style.width = "80%"
-};
+    document.querySelector(".story-detail-content").style.width = "80%";
+}
 
 function setImgWidth(){
     var storyImages = document.querySelectorAll(".detail-body img");
     for (var i=0; i < storyImages.length; i++){
-        var img = storyImages[i]
+        var img = storyImages[i];
 
-        img.style.width = "100%"
+        img.style.width = "100%";
         img.style.maxWidth = img.naturalWidth + 'px';
     }
 }
