64354ceb65be8ddf7e4cd5d115f1bf5ad1eb529b
Oleg Naumov <oleg.naumov@gmail.com>
2017-04-22 13:21:47 -0400

    Initial

---
+++ setWidth.js
@@ -0,0 +1,16 @@
+function setTextWidth(){
+    document.querySelector(".story-detail-content").style.width = "80%"
+};
+
+function setImgWidth(){
+    var storyImages = document.querySelectorAll(".detail-body img");
+    for (var i=0; i < storyImages.length; i++){
+        var img = storyImages[i]
+
+        img.style.width = "100%"
+        img.style.maxWidth = img.naturalWidth + 'px';
+    }
+}
+
+setTextWidth();
+setImgWidth();
